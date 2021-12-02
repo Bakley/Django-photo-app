898cc83433a1a68f88473a38f1c309f79fd780aa
barclaykoin <barclaykoin@gmail.com>
2021-12-02 14:14:44 +0300

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gallaryApp.wsgi --log-file -
---
+++ photos/views.py
@@ -0,0 +1,45 @@
+import six
+from cloudinary import api  # Only required for creating upload presets on the fly
+from django.shortcuts import render
+
+from .forms import PhotoForm
+from .models import Photo
+
+
+def filter_nones(d):
+    return dict((k, v) for k, v in six.iteritems(d) if v is not None)
+
+
+def list(request):
+    defaults = dict(format="jpg", height=150, width=150)
+    defaults["class"] = "thumbnail inline"
+
+    # The different transformations to present
+    samples = [
+        dict(crop="fill", radius=10),
+        dict(crop="scale"),
+        dict(crop="fit", format="png"),
+        dict(crop="thumb", gravity="face"),
+        dict(format="png", angle=20, height=None, width=None, transformation=[
+            dict(crop="fill", gravity="north", width=150, height=150, effect="sepia"),
+        ]),
+    ]
+    samples = [filter_nones(dict(defaults, **sample)) for sample in samples]
+    return render(request, 'list.html', dict(photos=Photo.objects.all(), samples=samples))
+
+
+def upload(request):
+    context = dict(
+        # Form demonstrating backend upload
+        backend_form=PhotoForm()
+    )
+
+    if request.method == 'POST':
+        # Only backend upload should be posting here
+        form = PhotoForm(request.POST, request.FILES)
+        context['posted'] = form.instance
+        if form.is_valid():
+            # Uploads image and creates a model instance for it
+            form.save()
+
+    return render(request, 'upload.html', context)
